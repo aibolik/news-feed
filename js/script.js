d518ed5a6e021e5431bd8acec93d3576102c5e0a
Aibol Kussain <Aibol_Kussain@epam.com>
2017-11-27 15:29:18 +0300

    feat: enable choosing endpoint for type of news

--- js/script.js
+++ js/script.js
@@ -2,8 +2,57 @@ const NEWS_API_HOST = 'https://newsapi.org/v2/';
 const API_KEY = '300ecf7f1d8c4128876d195675a1f16b';
 
 let state = {
-  sources: {}
+  endpoint: 'top-headlines',
+  sources: {},
+  getSelectedSources() {
+    let sources = [];
+    Object.keys(this.sources).map(key => {
+      if (state.sources[key].selected) {
+        sources.push(state.sources[key].id);
+      }
+    });
+    return sources;
+  }
 }
+
+// Requests
+
+getSourcesList = () => {
+  fetch(`${NEWS_API_HOST}sources?apiKey=${API_KEY}`)
+  .then(res => res.json())
+  .then(sources => {
+    let sourcesList = createSourcesList(sources);
+    let sourcesListNode = document.querySelector('.sources__list');
+    for (let node of sourcesList) {
+      sourcesListNode.appendChild(node);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  })
+}
+
+getNews = (sourcesList = ['google-news'], page = 1) => {
+  if (sourcesList.length === 0) {
+    sourcesList = ['google-news'];
+  }
+  let sources = sourcesList.join(',');
+  let url = `${NEWS_API_HOST}${state.endpoint}?apiKey=${API_KEY}&sources=${sources}&page=${page}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(news => {
+    let newsDomNodes = createNewsCards(news);
+    let newsListNode = document.getElementById('news-list');
+    for (let node of newsDomNodes) {
+      newsListNode.appendChild(node);
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+// View creators
 
 createSourceItem = ({ id, name, url }) => {
   state.sources[id] = {
@@ -22,77 +71,6 @@ createSourceItem = ({ id, name, url }) => {
 
 createSourcesList = sources => {
   return sources.sources.slice(0, 10).map(item => createSourceItem(item));
-}
-
-getSourcesList = () => {
-  fetch(`${NEWS_API_HOST}sources?apiKey=${API_KEY}`)
-  .then(res => res.json())
-  .then(sources => {
-    let sourcesList = createSourcesList(sources);
-    let sourcesListNode = document.querySelector('.sources__list');
-    for (let node of sourcesList) {
-      sourcesListNode.appendChild(node);
-    }
-  })
-  .catch(err => {
-    console.error(err);
-  })
-}
-
-getTopHeadlines = (sourcesList = ['google-news']) => {
-  let sources = sourcesList.join(',');
-  let url = `${NEWS_API_HOST}top-headlines?apiKey=${API_KEY}&sources=${sources}`;
-  fetch(url)
-  .then(res => res.json())
-  .then(news => {
-    let newsDomNodes = createNewsCards(news);
-    let newsListNode = document.getElementById("news-list");
-    for (let node of newsDomNodes) {
-      newsListNode.appendChild(node);
-    }
-
-  })
-  .catch(err => {
-    console.error(err);
-  });
-}
-
-getHumanReadableTime = (timeString) => {
-  let date = new Date(timeString);
-  return date.toDateString();
-}
-
-handleSourceClick = (e) => {
-  let target = e.target;
-  let dataSource = target.getAttribute('data-source');
-  target.classList.toggle('sources__item--active');
-  if (target.classList.contains('sources__item--active')) {
-    state.sources[dataSource].selected = true;
-  } else {
-    state.sources[dataSource].selected = false;
-  }
-  document.querySelector('.sources__btn').classList.add('sources__btn--visible');
-}
-
-applyFilter = (e) => {
-  let sources = [];
-  Object.keys(state.sources).map(key => {
-    if (state.sources[key].selected) {
-      sources.push(state.sources[key].id);
-    }
-  });
-  if (sources.length === 0) {
-    alert('Please, specify at least one source');
-    return;
-  }
-  let newsListNode = document.getElementById("news-list");
-  while(newsListNode.hasChildNodes()) {
-    newsListNode.removeChild(newsListNode.lastChild);
-  }
-  getTopHeadlines(sources);
-  e.target.classList.remove('sources__btn--visible');
-  document.querySelector('.sources__list').classList.remove('sources__list--opened');
-  document.querySelector('.sources__title .arrow').classList.remove('arrow--down');
 }
 
 createNewsCard = ({
@@ -130,38 +108,95 @@ createNewsCards = news => {
   return news.articles.map(newsItem => createNewsCard(newsItem));
 }
 
+// Event handlers
+
+handleSourceClick = (e) => {
+  let target = e.target;
+  let dataSource = target.getAttribute('data-source');
+  target.classList.toggle('sources__item--active');
+  if (target.classList.contains('sources__item--active')) {
+    state.sources[dataSource].selected = true;
+  } else {
+    state.sources[dataSource].selected = false;
+  }
+  document.querySelector('.sources__btn').classList.add('sources__btn--visible');
+}
+
+handleEndpointClick = (e) => {
+  e.preventDefault();
+  document.querySelector('.endpoint.endpoint--active').classList.remove('endpoint--active');
+  e.target.classList.add('endpoint--active');
+  state.endpoint = e.target.getAttribute('data-endpoint');
+  let newsListNode = document.getElementById("news-list");
+  while(newsListNode.hasChildNodes()) {
+    newsListNode.removeChild(newsListNode.lastChild);
+  }
+  let sources = state.getSelectedSources();
+  getNews(sources);
+}
+
+applyFilter = (e) => {
+  let sources = state.getSelectedSources();
+  if (sources.length === 0) {
+    alert('Please, specify at least one source');
+    return;
+  }
+  let newsListNode = document.getElementById("news-list");
+  while(newsListNode.hasChildNodes()) {
+    newsListNode.removeChild(newsListNode.lastChild);
+  }
+  getNews(sources);
+  e.target.classList.remove('sources__btn--visible');
+  document.querySelector('.sources__list').classList.remove('sources__list--opened');
+  document.querySelector('.sources__title .arrow').classList.remove('arrow--down');
+}
+
+// Utils
+
+getHumanReadableTime = (timeString) => {
+  let date = new Date(timeString);
+  return date.toDateString();
+}
+
+// Main body
+
+// attaching click handlers
+
 document.querySelector('.sources__btn').addEventListener('click', applyFilter);
 document.querySelector('.sources__title').addEventListener('click', e => {
   document.querySelector('.sources__list').classList.toggle('sources__list--opened');
   document.querySelector('.sources__title .arrow').classList.toggle('arrow--down');
 });
+let $endpoints = document.querySelectorAll('.endpoint');
+$endpoints.forEach(element => {
+  element.addEventListener('click', handleEndpointClick);
+});
+
+// attach click handler for mobile navigation
+
+// Get all "navbar-burger" elements
+let $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+// Check if there are any navbar burgers
+if ($navbarBurgers.length > 0) {
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach(function ($el) {
+    $el.addEventListener('click', function () {
+
+      // Get the target from the "data-target" attribute
+      let target = $el.dataset.target;
+      let $target = document.getElementById(target);
+
+      // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+      $el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+}
+
+// start requests
 
 getSourcesList();
-getTopHeadlines();
-
-
-document.addEventListener('DOMContentLoaded', function () {
-
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
-});
+getNews();
